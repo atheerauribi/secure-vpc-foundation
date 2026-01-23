37f408599e205dc4b0120155e4fe0038444e42c1
Atheer Auribi <atheer.auribi@gmail.com>
2026-01-22 23:35:26 -0500

    feat: added private NACLs

--- security.tf
+++ security.tf
@@ -183,3 +183,53 @@ resource "aws_network_acl_rule" "public_out_all" {
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
 }
+
+//Public NACL association
+resource "aws_network_acl_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  network_acl_id = aws_network_acl.public.id
+}
+
+//Private NACL
+resource "aws_network_acl" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-private-nacl"
+  }
+}
+
+//Inbound from VPC only
+resource "aws_network_acl_rule" "private_in_vpc" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 100
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = var.vpc_cidr
+  from_port      = 0
+  to_port        = 0
+}
+
+//Outbound all through NAT
+resource "aws_network_acl_rule" "private_out_all" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+}
+
+//Private NACL associations
+resource "aws_network_acl_association" "private_app" {
+  count          = length(aws_subnet.app)
+  subnet_id      = aws_subnet.app[count.index].id
+  network_acl_id = aws_network_acl.private.id
+}
+
+resource "aws_network_acl_association" "private_data" {
+  count          = length(aws_subnet.data)
+  subnet_id      = aws_subnet.data[count.index].id
+  network_acl_id = aws_network_acl.private.id
+}
