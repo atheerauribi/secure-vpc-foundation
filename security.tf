460347af7725976c028195116a0fa9605352e060
Atheer Auribi <atheer.auribi@gmail.com>
2026-01-24 18:41:44 -0500

    feat: added app sg egress rule to data sg on db port

--- security.tf
+++ security.tf
@@ -96,6 +96,16 @@ resource "aws_security_group_rule" "app_out_https" {
   security_group_id = aws_security_group.app.id
 }
 
+resource "aws_security_group_rule" "app_out_db_to_data" {
+  type                     = "egress"
+  from_port                = var.db_port
+  to_port                  = var.db_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.data.id
+  security_group_id        = aws_security_group.app.id
+}
+
+
 //Data sg
 resource "aws_security_group" "data" {
   name        = "${var.project_name}-data-sg"
