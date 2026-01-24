c8b017f7b3e8ffe81770a47c7b97ddc7f24d9d21
Atheer Auribi <atheer.auribi@gmail.com>
2026-01-24 16:30:12 -0500

    fix: fixed incorrect name for ami filtering

--- bastion.tf
+++ bastion.tf
@@ -5,7 +5,7 @@ data "aws_ami" "ubuntu_24" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
